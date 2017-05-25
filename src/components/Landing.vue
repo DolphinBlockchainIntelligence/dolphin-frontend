60489d9eb38e7a45659764238916fc89c8885727
Eugen Soloviov <suenot@gmail.com>
2017-05-25 21:21:25 +0300

    решил проблему в лендинге с неопределенной переменной

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,47 +1,47 @@
 <template>
 <div class="landing container">
   <section class="intro">
-    <h1>{{ landing.intro.title }}</h1>
-    <h3>{{ landing.intro.subtitle }}</h3>
-    <h4>{{ landing.intro.description }}</h4>
+    <h1>{{ intro.title }}</h1>
+    <h3>{{ intro.subtitle }}</h3>
+    <h4>{{ intro.description }}</h4>
   </section>
   <section class="about">
-    <h1>{{ landing.about.title }}</h1>
-    <h4>{{ landing.about.description }}</h4>
+    <h1>{{ about.title }}</h1>
+    <h4>{{ about.description }}</h4>
   </section>
   <section class="features">
-    <h1>{{ landing.features.title }}</h1>
+    <h1>{{ features.title }}</h1>
     <ul>
-      <li v-for="feature in landing.features.items">{{ feature }}</li>
+      <li v-for="feature in features.items">{{ feature }}</li>
     </ul>
   </section>
   <section class="screenshots">
-    <h1>{{ landing.screenshots.title }}</h1>
-    <h3>{{ landing.screenshots.subtitle }}</h3>
+    <h1>{{ screenshots.title }}</h1>
+    <h3>{{ screenshots.subtitle }}</h3>
   </section>
   <section class="roadmap">
-    <h1>{{ landing.roadmap.title }}</h1>
-    <h3>{{ landing.roadmap.subtitle }}</h3>
+    <h1>{{ roadmap.title }}</h1>
+    <h3>{{ roadmap.subtitle }}</h3>
     <ul>
-      <li v-for="stage in landing.roadmap.stages">{{ stage }}</li>
+      <li v-for="stage in roadmap.stages">{{ stage }}</li>
     </ul>
   </section>
   <section class="team">
-    <h1>{{ landing.team.title }}</h1>
-    <h3>{{ landing.team.subtitle }}</h3>
+    <h1>{{ team.title }}</h1>
+    <h3>{{ team.subtitle }}</h3>
     <ul>
-      <li v-for="member in landing.team.members">{{ member }}</li>
+      <li v-for="member in team.members">{{ member }}</li>
     </ul>
   </section>
   <section class="mentions">
-    <h1>{{ landing.mentions.title }}</h1>
+    <h1>{{ mentions.title }}</h1>
     <ul>
-      <li v-for="mention in landing.mentions.items">{{ mention }}</li>
+      <li v-for="mention in mentions.items">{{ mention }}</li>
     </ul>
   </section>
   <section class="socials">
     <ul>
-      <li v-for="social in landing.socials">{{ social }}</li>
+      <li v-for="social in socials">{{ social }}</li>
     </ul>
   </section>
 </div>
@@ -52,13 +52,26 @@ import axios from 'axios'
 export default {
   name: 'landing',
   data: () => ({
-    landing: []
+    intro: [],
+    about: [],
+    features: [],
+    screenshots: [],
+    roadmap: [],
+    team: [],
+    mentions: [],
+    socials: []
   }),
   created () {
     axios.get('/static/data/landing.json')
     .then(response => {
-      this.landing = response.data
-      console.log(this.landing)
+      this.intro = response.data.intro
+      this.about = response.data.about
+      this.features = response.data.features
+      this.screenshots = response.data.screenshots
+      this.roadmap = response.data.roadmap
+      this.team = response.data.team
+      this.mentions = response.data.mentions
+      this.socials = response.data.socials
     })
     .catch(e => {
       this.errors.push(e)
