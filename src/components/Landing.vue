c838c32f4fca089fb868b47eaec6a9a77fb933b0
Eugen Soloviov <suenot@gmail.com>
2017-05-30 08:36:57 +0300

    улучшения в лендинге

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,60 +1,73 @@
 <template lang="pug">
   .landing
-    section.section.intro
-      .container
-        img(:src="intro.logo" class="logo")
+    section.section.intro: .container
+      img(:src="intro.logo" class="logo")
+      .heading
         h1.title {{ intro.title }}
         h3.subtitle {{ intro.subtitle }}
-        p.description {{ intro.description }}
-    section.section.about
-      .container
+      p.description {{ intro.description }}
+    section.section.about: .container
+      .heading
         h3.title {{ about.title }}
-        p.description {{ about.description }}
-        p.text-center: b-button(:size="'lg'" :variant="'outline-success'" href="https://goo.gl/fuMCKU") Read the whitepaper
-    section.section.features
-      .container
+      p.description {{ about.description }}
+      p.text-center: b-button(:size="'lg'" :variant="'outline-success'" href="https://goo.gl/fuMCKU") Read the whitepaper
+    section.section.features: .container
+      .heading
         h3.title {{ features.title }}
-        ul.list
-          li.item(v-for="feature in features.items")
-            div.item-img(:style="'background-image: url(' + feature.img + ')'")
-            h4.item-title {{ feature.title }}
-            p.item-description {{ feature.description }}
-    section.section.screenshots
-      .container
+      ul.list: li.item(v-for="feature in features.items")
+        div.item-img(:style="'background-image: url(' + feature.img + ')'")
+        h4.item-title {{ feature.title }}
+        p.item-description {{ feature.description }}
+    section.section.screenshots: .container
+      .heading
         h3.title {{ screenshots.title }}
         h4.subtitle {{ screenshots.subtitle }}
-        img(:src="screenshots.imgs[0]")
-    section.section.roadmap
-      .container
+      img(:src="screenshots.imgs[0]")
+    section.section.roadmap: .container
+      .heading
         h3.title {{ roadmap.title }}
         h4.subtitle {{ roadmap.subtitle }}
-        ul: li(v-for="stage in roadmap.stages")
-          h5 {{ stage.title }}
-          ul: li(v-for="goal in stage.goals") {{ goal }}
-    section.section.team
-      .container
+      ul: li(v-for="stage in roadmap.stages")
+        h5 {{ stage.title }}
+        ul: li(v-for="goal in stage.goals") {{ goal }}
+    section.section.team: .container
+      .heading
         h3.title {{ team.title }}
         h4.subtitle {{ team.subtitle }}
-        ul: li(v-for="member in team.members")
-          div.item-img(:style="'background-image: url(' + member.img + ')'")
+      ul.row: li.col-xs-12.col-md-6.col-lg-3(v-for="member in team.members")
+        b-card.mb-2
+          //- div.item-img(:style="'background-image: url(' + member.img + ')'")
+          img(:src="member.img" :alt="member.name")
           h5 {{ member.name }}
           div {{ member.role }}
           ul: li(v-for="description in member.description") {{ description }}
           ul: li(v-for="link in member.links")
             a(:href="link.url") {{ link.name }}
-    section.section.mentions
-      .container
+    section.section.mentions: .container
+      .heading
         h3.title {{ mentions.title }}
-        ul.items: li(v-for="mention in mentions.items")
-          a(:href="mention.url"): img(:src="mention.img" :alt="mention.title")
-    section.section.socials
-      .container
-        ul
-          li(v-for="social in socials")
-            a(href="social.url") {{ social.title }}
+      ul.items: li(v-for="mention in mentions.items")
+        a(:href="mention.url"): img(:src="mention.img" :alt="mention.title")
+    sections.section.subscription: .container
+      p.text-center: b-button(:size="'lg'" :variant="'outline-success'" href="http://eepurl.com/cQEwDT" target="_blank") Subscribe us
+    section.section.socials: .container
+      //- ul: li(v-for="social in socials"): a(:href="social.url") {{ social.title }}
+      ul
+        li: a(href="https://www.facebook.com/groups/dolphinblockchainintelligence/" target="_blank"): SocialFb
+        li: a(href="https://twitter.com/dolphin_bi" target="_blank"): SocialTw
+        li: a(href="https://www.linkedin.com/company/dolphin-blockchain-intelligence" target="_blank"): SocialLn
+        li: a(href="https://github.com/DolphinBlockchainIntelligence" target="_blank"): SocialGh
+        li: a(href="https://t.me/joinchat/AAAAAArUuUw5_lK-v9Vw7A" target="_blank"): SocialTg
+        li: a(href="#" target="_blank"): SocialVk
 </template>
 
 <script>
+import SocialFb from '../assets/img/socials/fb.svg'
+import SocialGh from '../assets/img/socials/gh.svg'
+import SocialLn from '../assets/img/socials/ln.svg'
+import SocialTg from '../assets/img/socials/tg.svg'
+import SocialTw from '../assets/img/socials/tw.svg'
+import SocialVk from '../assets/img/socials/vk.svg'
 import axios from 'axios'
 export default {
   name: 'landing',
@@ -68,6 +81,14 @@ export default {
     mentions: [],
     socials: {}
   }),
+  components: {
+    SocialFb,
+    SocialGh,
+    SocialLn,
+    SocialTg,
+    SocialTw,
+    SocialVk
+  },
   created () {
     axios.get('/static/data/landing.json')
     .then(response => {
@@ -88,15 +109,17 @@ export default {
 </script>
 
 <style lang="sass">
+@import '../assets/grid-breakpoints.scss'
 .landing
   margin-top: -56px !important
 .section
   padding: 50px 0
-  .title
-    text-align: center
+  .heading
     margin-bottom: 30px
-  .subtitle
-    text-align: center
+    .title
+      text-align: center
+    .subtitle
+      text-align: center
 .intro
   min-width: 100vw
   min-height: 100vh
@@ -154,11 +177,24 @@ export default {
   img
     width: 100%
 .team
-  .item-img
-    width: 150px
-    height: 150px
-    background-size: cover
-    margin-bottom: 30px
+  ul
+    // display: flex
+    li
+      list-style: none
+      // padding: 0
+  img
+    margin: -20px 0 20px -20px
+    width: 100%
+    width: calc(100% + 40px)
+    // +media-breakpoint-down(sm)
+  // .item-img
+  //   width: 100%
+  //   height: 203px
+  //   background-size: cover
+  //   margin-bottom: 30px
+  //   +media-breakpoint-only(xs)
+  //     height: auto
+  //     width: auto
 .mentions
   .items
     list-style: none
@@ -168,4 +204,17 @@ export default {
     align-items: center
     img
       height: 75px
+.socials
+  ul
+    list-style: none
+    display: flex
+    justify-content: center
+    li
+      margin: 0 20px
+  .social
+    height: 80px
+    width: 80px
+    opacity: 0.7
+    &:hover
+      opacity: 1
 </style>
