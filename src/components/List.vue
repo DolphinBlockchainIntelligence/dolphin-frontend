fabb6c527903e74de60325abc6b62e97b1c2c402
Eugen Soloviov <suenot@gmail.com>
2017-06-05 12:01:46 +0300

    basic functional: sort, search

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,60 +1,50 @@
 <template lang="pug">
   .container.list
     h3 {{$t("coinsList")}}
-    button(v-on:click="shuffle") Shuffle
-    input(v-model="query")
+    b-form-input(v-model="query")
     table.table.table-hover.table-striped
       thead
         tr
-          td: strong Announce
-          td: strong Replies
-          td: strong Views
+          td(v-on:click="sort('announce')")
+            span Announce
+            i.material-icons
+          td(v-on:click="sort('NumReplies')")
+            span Replies
+            i.material-icons
+          td(v-on:click="sort('NumViews')")
+            span Views
+            i.material-icons
           td
       tbody(name="table-row")
-        tr(v-for="coin in computedList" key="tr" class="table-row-item")
+        tr(v-for="coin in computedList" key="tr" class="table-row-item" :to="'/coin/' + coin.topicId" v-on:click="goToCoin(coin.topicId)")
           td(key="announce") {{ coin.announce }}
           td(key="replies") {{ coin.NumReplies }}
-          td(key="links") {{ coin.NumReplies }}
+          td(key="views") {{ coin.NumViews }}
           td.links(key="links")
-            a(v-on:click="dataHref(props.row.topicStarterUrl, $event)")
+            a(v-on:click="dataHref(coin.topicStarterUrl, $event)")
               i.material-icons account_circle
-            a(v-on:click="dataHref(props.row.topicUrl, $event)")
+            a(v-on:click="dataHref(coin.topicUrl, $event)")
               i.material-icons assignment
 </template>
 
 <script>
 import _ from 'lodash'
 import Vue from 'vue'
-import axios from 'axios'
-import {ClientTable, Event} from 'vue-tables-2'
 import routes from '../router'
-Vue.use(ClientTable)
-
 import firebase from '../firebase'
 
 export default {
   name: 'list',
   data: () => ({
     query: '',
-    coinsList: [],
-    columns: ['announce', 'NumReplies', 'NumViews', 'links'],
-    options: {
-      headings: {
-        announce: 'Announce',
-        NumReplies: 'Replies',
-        NumViews: 'Views',
-        links: ''
-      }
-    }
+    sortBy: '',
+    sortOrder: '',
+    coinsList: []
   }),
   created () {
     let component = this
     firebase.database().ref('/coinsList/').once('value').then(function(snapshot) {
       component.coinsList = snapshot.val()
-    })
-    Event.$on('vue-tables.row-click', function (data) {
-      console.log('FIREFOX')
-      routes.push({ name: 'Coin', params: { id: data.row.topicId }})
     })
   },
   methods: {
@@ -63,42 +53,65 @@ export default {
       event.stopPropagation()
       window.open(url, '_blank')
     },
-    shuffle: function () {
-      this.coinsList = _.shuffle(this.coinsList)
+    goToCoin(url) {
+      routes.push({ name: 'Coin', params: { id: url }})
+    },
+    sort: function (sortBy) {
+      this.sortBy = sortBy
+      let classList = event.currentTarget.querySelector('i').classList
+      let siblings = document.querySelectorAll('.table thead tr .material-icons').forEach(function(item) {
+        item.classList.remove('up')
+        item.classList.remove('down')
+      })
+      if (this.sortOrder == 'asc') {
+        this.sortOrder = 'desc'
+        // console.log(event.currentTarget)
+        classList.remove('down')
+        classList.add('up')
+        // .forEach(function(item) {
+          // item.classList.toggle('up')
+        // })
+      } else if (this.sortOrder == 'desc') {
+        this.sortOrder = 'asc'
+        classList.remove('up')
+        classList.add('down')
+      } else {
+        this.sortOrder = 'asc'
+        classList.remove('down')
+        classList.add('up')
+      }
     }
   },
   computed: {
     computedList: function () {
-      var vm = this
-      return this.coinsList.filter(function (item) {
+      let vm = this
+      let list = this.coinsList.filter(function (item) {
         return item.announce.toLowerCase().indexOf(vm.query.toLowerCase()) !== -1
       })
+      if (this.sortBy) {
+        return _.orderBy(list, [this.sortBy], [this.sortOrder])
+      } else {
+        return _.orderBy(list, ['NumReplies', 'NumViews', 'announce'], ['desc', 'desc', 'asc'])
+      }
     }
   }
 }
 </script>
 
 <style lang="sass">
-.VueTables
-  tr
+.table
+  thead tr td
+    user-select: none
+    font-weight: bold
+    span
+      vertical-align: middle
+    .material-icons
+      vertical-align: middle
+      font-size: 16px
+      &.down::after
+        content: 'arrow_downward'
+      &.up::after
+        content: 'arrow_upward'
+  tbody tr
     cursor: pointer
-  .col-md-6
-    &:nth-child(2) > div
-      display: flex
-      justify-content: flex-end
-    label
-      margin-right: 7px
-  .links
-    white-space: nowrap
-.VuePagination__pagination
-  justify-content: center
-.VuePagination__count
-  display: none
-
-.table-row-enter,
-.table-row-leave-active,
-.table-row-move
-  transition: transform 3s
-.table-row-item-move
-  backface-visibility: hidden
 </style>
