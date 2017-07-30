47037d2820f276f032220d3760ac59a9d8972d96
Eugen Soloviov <suenot@gmail.com>
2017-07-30 12:22:49 +0300

    test icoface

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,14 @@
           span Dolphin BI
         b-collapse#nav_collapse(is-nav='')
           b-nav(is-nav-bar='')
-            b-nav-item(to='/list') BTT sentiments (alpha)
-            b-nav-item(href='/icoface') ICOface
+            b-nav-item(to='/#/list') BTT sentiments
+            b-nav-item(href='/#/icoface') ICOface
     router-view
-  //footer.footer
-    .container
-      .left
-        span © 2017 Dolphin BI | <a href="https://docs.google.com/forms/d/e/1FAIpQLSd6mlhhRCTuMIsdi2kYZP8RjEO4w2ytpcVFtDnNXyLusfcnMA/viewform?fbzx=-2801639494221500400">Request Form</a> | <a href="#">BitcoinTalk</a>
-      .right
-        span Donate BTC: <a href="#">0000000000000000000000000000000000</a>
-        br
-        span Donate ETH: <a href="#">0x0000000000000000000000000000000000000000</a>
 </template>
 
 <script>
 import $ from 'jquery'
-// let $ = jQuery = require('jquery');
 import Vue from 'vue'
-// import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.js'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -40,7 +30,6 @@ export default window.App
 </script>
 
 <style lang="sass">
-// @import './assets/sticky-footer.sass'
 body
   overflow-x: hidden
 #app
@@ -53,17 +42,4 @@ body
       margin-top: 20px
 .page-title
   margin-bottom: 20px
-
-// .footer
-//   padding: 10px 0
-//   margin-top: 10px
-//   font-size: 13px
-//   border-top: 1px solid #DDDDEE
-//   .container
-//     display: flex
-//     align-items: center
-//     .left
-//       flex: 1 0 auto
-//     .right
-//       text-align: right
 </style>
