fd5bb3f62f2e2ead11566c395964699e077791f3
Eugen Soloviov <suenot@gmail.com>
2017-08-22 21:37:58 +0300

    dashboard-lite

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -1,53 +1,54 @@
 <template>
-  <main class="mdl-layout__content">
-    <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--12-col">
-        <div class="heading-box">
-          <h4 class="left">{{ announce }}</h4>
-          <div class="right">
-            <a :href="topicStarterUrl" target="_blank"><i class="material-icons">account_circle</i></a>
-            <a :href="topicUrl" target="_blank"><i class="material-icons">assignment</i></a>
-            <a href="#" @click.prevent="toggleFavorite($event)"><i class="material-icons">star_border</i></a>
-            <a href="#" @click.prevent="toggleSettings()"><i class="material-icons">settings</i></a>
+  <grid-layout
+    :layout="widgets"
+    :col-num="12"
+    :row-height="30"
+    :is-draggable="true"
+    :is-resizable="true"
+    :margin="[0, 0]"
+    :use-css-transforms="true"
+  >
+    <grid-item v-for="item in widgets"
+      :x="item.x"
+      :y="item.y"
+      :w="item.w"
+      :h="item.h"
+      :i="item.i"
+      :key="item.id"
+      @resized="resizedWidget()">
+        <div class="widget" :is="item.name">
+          <!-- <div class="widget-header">{{item.title}}</div> -->
+          <div class="iframe-wrapper">
+            <iframe :src="item.src" scrolling="no" />
           </div>
         </div>
-      </div>
-    </div>
-    <div class="mdl-grid" id="draggable-container">
-      <template v-for="(child, index) in widgets">
-        <component :is="child.name" :key="child.name" :id="child.id"></component>
-      </template>
-    </div>
-  </main>
+    </grid-item>
+  </grid-layout>
 </template>
 
 <script>
+let testLayout = [
+  {'x':0,'y':0,'w':12,'h':10,'i':'0','name': 'SentimentsLineChart', 'title': 'SentimentsLineChart', 'src': 'http://178.218.115.169:5003/#/experts-evaluations/1'},
+  {'x':0,'y':10,'w':12,'h':10,'i':'1','name': 'SentimentsComments', 'title': 'SentimentsComments', 'src': 'http://178.218.115.169:5003/#/experts-evaluations/1'},
+  {'x':0,'y':20,'w':12,'h':10,'i':'2','name': 'ExpertsEvaluations', 'title': 'ExpertsEvaluations', 'src': 'http://178.218.115.169:5003/#/experts-evaluations/1'}
+];
+import VueGridLayout from 'vue-grid-layout'
+let GridLayout = VueGridLayout.GridLayout
+let GridItem = VueGridLayout.GridItem
+
 import axios from 'axios'
 import { mapState } from 'vuex'
-const Sortable = require('sortablejs')
-
-import SentimentsPieChart from './widgets/SentimentsPieChart.vue'
-import SentimentsLineChart from './widgets/SentimentsLineChart.vue'
-import SentimentsComments from './widgets/SentimentsComments.vue'
-import FacesSearch from './widgets/FacesSearch.vue'
-import FacesProject from './widgets/FacesProject.vue'
-import ExpertsEvaluations from './widgets/ExpertsEvaluations.vue'
-
 export default {
   name: 'post',
   data: () => ({
     id: '',
-    widgets: [{'id': 3, 'name': 'ExpertsEvaluations'}, {'id': 1, 'name': 'SentimentsLineChart'}, {'id': 2, 'name': 'SentimentsComments'}],
+    widgets: testLayout,
     topicStarterUrl: '',
-    topicUrl: ''
+    topicUrl: '',
   }),
   components: {
-    SentimentsLineChart,
-    SentimentsPieChart,
-    SentimentsComments,
-    FacesSearch,
-    FacesProject,
-    ExpertsEvaluations
+    GridLayout,
+    GridItem
   },
   created () {
     // this.searchEsc()
@@ -55,13 +56,11 @@ export default {
   mounted () {
     // TODO: Add and remove widget
     this.$root.$on('addWidget', (widgetName) => {
-      this.widgets.push({'id': this.widgets.length+1, 'name': widgetName})
+      this.widgets.push({'i': this.widgets.length+1, 'name': widgetName, 'title': 'Widget name', 'x': 0, 'y': 0, 'w': 12, 'h': 10})
     })
-    this.$root.$on('removeWidget', (id) => {
-      this.widgets = _.reject(this.widgets, { 'id': id })
+    this.$root.$on('removeWidget', (i) => {
+      this.widgets = _.reject(this.widgets, { 'i': i })
     })
-    //
-    this.initSortable()
     this.clearSearchResults()
   },
   computed: {
@@ -100,97 +99,36 @@ export default {
         event.target.innerHTML = 'star'
       }
     },
-    initSortable: () => {
-      const el = document.getElementById('draggable-container')
-      const sortable = Sortable.create(el, {
-        handle: ".btn-drag"
-      })
-    },
     clearSearchResults: () => {
       document.getElementById('search-icon-clear').classList.add('hide')
       document.getElementById('search-icon').classList.remove('hide')
+    },
+    resizedWidget: () => {
+      window.dispatchEvent(new Event('resize'))
     }
   }
 }
 </script>
 
 <style lang="sass">
-// widget
-.widget-content
-  padding: 0
-  margin: 0 10px
-
-.heading-box
-  display: flex
-  align-items: center
-  .left
-    flex: 1 1 auto
-  .right
-    display: flex
-    a
-      color: #3f51b5
-      margin: 24px 0 16px
-      opacity: .7
-      margin-left: 7px
-      &:hover
-        opacity: 1
-.widget-faces-search
-  .search-bar
-    display: flex
-    input
-      flex: 1 0 auto
-  .bootstrap-input
-    padding: .5rem .75rem
-    font-size: 1rem
-    line-height: 1.25
-    border: 1px solid rgba(0,0,0,.15)
-    border-radius: 2px
+.vue-grid-item
+  border: 1px solid #ddd
 .widget
-  height: 460px
-  overflow-y: scroll
-.widget-faces
-  ul
-    padding: 0
-    margin: 0
-.widget-comments
-  height: 460px
-  overflow-y: scroll
-  // padding-top: 28px
-.mdl-card
-  width: 100% !important
-  .mdl-card__title
-    flex: 0 0 auto
-.comments
-  list-style: none
-  padding: 0
-  margin: 0 10px
-  .comment
-    margin-bottom: 20px
-    .heading
-      display: flex
-      justify-content: space-between
-      color: #999
-    .text
-      padding: 7px 14px
-      border-radius: 2px
-      display: block
-      color: #292b2c
-      text-decoration: none
-      cursor: pointer
-      opacity: .8
-      &:hover
-        opacity: 1
-.sentiment0 .text
-  background: #f98a83
-.sentiment1 .text
-  background: #dddddd
-.sentiment2 .text
-  background: #85f77e
-.nodata
-  width: 100%
-  height: 400px
-  display: flex
-  justify-content: center
-  align-items: center
-  border: 1px solid #ccc
+  position: absolute
+  top: 0
+  left: 0
+  right: 0
+  bottom: 0
+  padding: 0 50px
+  background: white
+  .iframe-wrapper
+    overflow: hidden
+    width: 100%
+    height: 100%
+    iframe
+      width: 100%
+      height: 100%
+      overflow: hidden
+      html, body
+        overflow: hidden !important
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import List from '@/components/List'
-import Post from '@/components/Post'
-import Store from '@/components/Store'
-import ICOFace from '@/components/ICOFace'
-import SentimentsComments from '@/components/widgets/SentimentsComments'
-import SentimentsLineChart from '@/components/widgets/SentimentsLineChart'
-import SentimentsPieChart from '@/components/widgets/SentimentsPieChart'
-import ExpertsEvaluations from '@/components/widgets/ExpertsEvaluations'
-import SentimentsBritechartsDonut from '@/components/widgets/SentimentsBritechartsDonut'
-import SentimentsBritechartsLine from '@/components/widgets/SentimentsBritechartsLine'
-import SentimentsBritechartsStackedArea from '@/components/widgets/SentimentsBritechartsStackedArea'
-
+import Dashboard from '@/components/Dashboard'
 
 
 Vue.use(Router)
@@ -19,74 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'List',
-      component: List
-    },
-    {
-      path: '/sentiments-comments/:id',
-      name: 'SentimentsComments',
-      component: SentimentsComments,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/sentiments-line-chart/:id',
-      name: 'SentimentsLineChart',
-      component: SentimentsLineChart,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/sentiments-pie-chart/:id',
-      name: 'SentimentsPieChart',
-      component: SentimentsPieChart,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/experts-evaluations/:id',
-      name: 'ExpertsEvaluations',
-      component: ExpertsEvaluations,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/sentiments-britechart-donut/:id',
-      name: 'SentimentsBritechartsDonut',
-      component: SentimentsBritechartsDonut,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/sentiments-britechart-line/:id',
-      name: 'SentimentsBritechartsLine',
-      component: SentimentsBritechartsLine,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/sentiments-britechart-stacked-area/:id',
-      name: 'SentimentsBritechartsStackedArea',
-      component: SentimentsBritechartsStackedArea,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/post/:id',
-      name: 'Post',
-      component: Post,
-      props: true,
-      meta: { reuse: false }
-    },
-    {
-      path: '/store/',
-      name: 'Store',
-      component: Store
-    },
-    {
-      path: '/icoface/',
-      name: 'ICOFace',
-      component: ICOFace
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
