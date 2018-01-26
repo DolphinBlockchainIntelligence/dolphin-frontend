3762d3b23fbe01449e1ee1053225db8facc2407b
aevsyukov <aevsyukov@urbgroup.ru>
2018-01-26 18:04:06 +0300

    Add auth lock screen

--- client/src/components/UserProfile.vue
+++ client/src/components/UserProfile.vue
@@ -1,48 +1,58 @@
 <template>
-  <div class="container">
-    <div class="heading">
-      <h3 class="heading-text">Profile</h3>
-    </div>
-    <br>
-    <div class="form-horizontal">
-      <div class="form-group row">
-        <label class="col-md-2 control-label">Name</label>
-        <div class="col-md-10">
-          <input type="text" class="form-control" v-model="userProfile.realName">
+  <div>
+    <div v-if="user">
+      <div class="container">
+        <div class="heading">
+          <h3 class="heading-text">Profile</h3>
         </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-md-2 control-label">Profile</label>
-        <div class="col-md-10">
+        <br>
+        <div class="form-horizontal">
+          <div class="form-group row">
+            <label class="col-md-2 control-label">Name</label>
+            <div class="col-md-10">
+              <input type="text" class="form-control" v-model="userProfile.realName">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label class="col-md-2 control-label">Profile</label>
+            <div class="col-md-10">
                     <textarea class="form-control"
                               v-model="userProfile.profileText">{{userProfile.profileText}}</textarea>
-        </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-md-2 control-label">Photo</label>
-        <div class="col-md-10">
-          <b-form-file v-model="userProfile.photo"  @change="onFileChange" accept=".jpg, .jpeg, .png, .gif" placeholder="Choose a file..."></b-form-file>
-          <div v-if="fileError"><small class="form-text text-muted alert alert-danger">{{ fileError }}.</small></div>
-          <div v-if="userProfile.photoUrl">
-            <img :src="userProfile.photoUrl" height="100"/>
+            </div>
           </div>
-        </div>
-      </div>
-      <div v-for="(socialLink, key) in userProfile['social']">
-        <div class="form-group row">
-          <label class="col-md-2 control-label">{{key}}</label>
-          <div class="col-md-10">
-            <input type="text" class="form-control" v-model="userProfile.social[key]">
+          <div class="form-group row">
+            <label class="col-md-2 control-label">Photo</label>
+            <div class="col-md-10">
+              <b-form-file v-model="userProfile.photo" @change="onFileChange" accept=".jpg, .jpeg, .png, .gif"
+                           placeholder="Choose a file..."></b-form-file>
+              <div v-if="fileError">
+                <small class="form-text text-muted alert alert-danger">{{ fileError }}.</small>
+              </div>
+              <div v-if="userProfile.photoUrl">
+                <img :src="userProfile.photoUrl" height="100"/>
+              </div>
+            </div>
           </div>
+          <div v-for="(socialLink, key) in userProfile['social']">
+            <div class="form-group row">
+              <label class="col-md-2 control-label">{{key}}</label>
+              <div class="col-md-10">
+                <input type="text" class="form-control" v-model="userProfile.social[key]">
+              </div>
+            </div>
+          </div>
+          <button class="btn btn-primary" v-bind:class="{preLoader: preLoader, disabled:!showSave}"
+                  @click.prevent="saveChanges">
+            <span v-if="preLoader">Loading...</span>
+            <span v-else>Save</span>
+          </button>
         </div>
       </div>
-      <button class="btn btn-primary" v-bind:class="{preLoader: preLoader, disabled:!showSave}"
-              @click.prevent="saveChanges">
-        <span v-if="preLoader">Loading...</span>
-        <span v-else>Save</span>
-      </button>
+      <br>
     </div>
-    <br>
+    <div v-else>
+      <auth/>
+    </div>
   </div>
 </template>
 
@@ -51,24 +61,31 @@
   import {mapState} from 'vuex'
   import axios from 'axios'
   import Navbar from './blocks/Navbar'
+  import Auth from './Auth'
 
   export default {
     name: 'userprofile',
     props: ['id'],
+    components: {Auth},
+    computed: {
+      ...mapState([
+        'user'
+      ])
+    },
     data: () => ({
 //      userProfile: false,
       userProfile: {
-        "realName": "Eugen Soloviov",
-        "photo": null,
-        "photoUrl": null,
-        "profileText": "lalala",
+        "realName": "",
+        "photo": "",
+        "photoUrl": "",
+        "profileText": "",
         "social": {
-          "Facebook": 'https://www.facebook.com/ArtemEVS',
-          'Twitter': 'https://twitter/123',
-          'Reddit': null,
-          'Github': null,
-          'Youtube': null,
-          'Bitcointalk': null
+          "Facebook": "",
+          'Twitter': "",
+          'Reddit': "",
+          'Github': "",
+          'Youtube': "",
+          'Bitcointalk': ""
         }
       },
       showSave: false,
@@ -117,10 +134,10 @@
           data.append('Bitcointalk', this.userProfile.social.Bitcointalk);
 
           var config = {
-            onUploadProgress: function(progressEvent) {
-              var percentCompleted = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+            onUploadProgress: function (progressEvent) {
+              var percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
             },
-            headers: { 'Content-Type': 'multipart/form-data' }
+            headers: {'Content-Type': 'multipart/form-data'}
           };
 
           axios.post(
@@ -137,7 +154,7 @@
           })
         }
       },
-      onFileChange (event) {
+      onFileChange(event) {
         this.fileError = null
         console.log(event.target.files[0].size)
         if (event.target.files[0].size > 8000000) {
