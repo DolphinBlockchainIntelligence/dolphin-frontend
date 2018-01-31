696654a603210c89f0a2b5d5a134735472fc95b4
Serghey Soloviov <thatmean@gmail.com>
2018-01-31 21:13:34 +0200

    opinion add fix

--- client/src/components/ExpertOpinionAdd.vue
+++ client/src/components/ExpertOpinionAdd.vue
@@ -7,31 +7,31 @@
     <form @submit.prevent="onSubmit()">
       <div class="form-group">
         <label>Idea</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="idea"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="idea"></star-rating>
       </div>
       <div class="form-group">
         <label>Team</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="team"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="team"></star-rating>
       </div>
       <div class="form-group">
         <label>Busmodel</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="busmodel"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="busmodel"></star-rating>
       </div>
       <div class="form-group">
         <label>Market</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="market"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="market"></star-rating>
       </div>
       <div class="form-group">
         <label>Tech</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="tech"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="tech"></star-rating>
       </div>
       <div class="form-group">
         <label>Code</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="code"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="code"></star-rating>
       </div>
       <div class="form-group">
         <label>Legal</label>
-        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" v-model="legal"></star-rating>
+        <star-rating :increment="1" :fixed-points="2" :read-only="false" :star-size="28" :border-width="1" border-color="#3f51b5" inactive-color="#fff" active-color="#3f51b5" :max-rating="10" v-model="legal"></star-rating>
       </div>
 
       <div class="form-group">
