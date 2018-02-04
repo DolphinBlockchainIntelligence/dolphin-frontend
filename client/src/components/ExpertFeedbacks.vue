cf83f9797a77ceddd115373e4b6db35e0122f179
Serghey Soloviov <thatmean@gmail.com>
2018-02-04 20:17:33 +0200

    expert reviews-feedbacks

---
+++ client/src/components/ExpertFeedbacks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container">
+      
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'feedbacks',
+  mounted: function() {
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
